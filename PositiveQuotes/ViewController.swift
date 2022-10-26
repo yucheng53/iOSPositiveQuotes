55c005d98a721694bdc9efb562af451139cacc1a
Cheng <yucheng618@gmail.com>
2022-10-26 14:26:47 -0400

    first init

--- PositiveQuotes/ViewController.swift
+++ PositiveQuotes/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var lable: UILabel!
+    
+    let arrayOfQuotes = ["You are awesome", "Pretty cool", "Have a good weekend","Beatiful sunday"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let randomNumber = Int.random(in: 0...arrayOfQuotes.count - 1)
+        lable.text = arrayOfQuotes[randomNumber]
+    }
 }
 
